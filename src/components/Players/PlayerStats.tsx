f2512aef1435ff8dbcfecb99311008bb8e13d0e3
Benjamin Henke <benjaminmhenke@gmail.com>
2025-08-07 14:30:34 -0500

    Players folder structure, added HP.css

---
+++ src/components/Players/PlayerStats.tsx
@@ -0,0 +1,6 @@
+import { useState, useEffect } from 'react';
+import { supabase } from '../../SupabaseClient';
+import './PlayerStats.css'
+
+import Hp from './Stats/HP/index'
+import Stress from './Stats/Stress'
