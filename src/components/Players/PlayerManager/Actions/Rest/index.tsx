0befae19b96c0f73462fedb1d76f122a65fd8b31
Benjamin Henke <benjaminmhenke@gmail.com>
2025-08-11 10:32:06 -0500

    init steps for Rest functionality

---
+++ src/components/Players/PlayerManager/Actions/Rest/index.tsx
@@ -0,0 +1,13 @@
+import { useState, useRef, useEffect } from "react";
+import { Flame } from "lucide-react";
+
+const Rest = ({ onLongRest, onShortRest }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const buttonRef = useRef(null);
+
+
+
+};
+
+export default Rest;
