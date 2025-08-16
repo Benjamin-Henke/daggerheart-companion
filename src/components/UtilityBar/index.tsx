4dd56a8d81298f17097e8b54aaa77b393ffb9461
Benjamin Henke <benjaminmhenke@gmail.com>
2025-08-16 00:13:07 -0500

    added new logo

--- src/components/UtilityBar/index.tsx
+++ src/components/UtilityBar/index.tsx
@@ -2,6 +2,7 @@ import AddPlayer from './Actions/AddPlayer';
 import Rest from './Actions/Rest';
 import type { Player } from '../../types/Player';
 import './UtilityBar.css';
+import logo from '../../assets/DC-logo.png'
 
 interface UtilityBarProps {
   onPlayerAdded: (newPlayer: Player) => void;
@@ -13,6 +14,7 @@ const UtilityBar = ({ onPlayerAdded, onError }: UtilityBarProps) => {
     <nav className="utility-bar">
       <div className="utility-bar-content">
         {/*<h1 className="utility-bar-title">DAGGERHEART COMPANION</h1>*/}
+        <img src={logo} alt="logo" className="logo" />
         <div className="utility-bar-actions">
           <Rest />
           <AddPlayer onPlayerAdded={onPlayerAdded} onError={onError} />
